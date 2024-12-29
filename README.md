fe257be4ed53d72ed0a61fc333e2e6f57369ea9a
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2024-12-29 12:41:01 +0400

    chore: Add a few readme and build command

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tetris in C without lib
+
+The project is to build a tetris game, in C, without much libraries. Allowed
+the C stdlib and linux system call, and all graphical will be done by using
+direct access to linux framebuffer.
+
+# Run
+
+Run `nix run` in a linux terminal (not under X).
--- flake.nix
+++ flake.nix
@@ -7,5 +7,13 @@
     devShells.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
       buildInputs = [ clang ccls ];
     };
+    packages.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux; runCommand "tetris"
+      {
+        buildInputs = [ clang ];
+      }
+      ''
+        mkdir -p $out/bin
+        clang -O3 ${./tetris.c} -o $out/bin/tetris;
+      '';
   };
 }
